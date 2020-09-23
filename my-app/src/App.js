542ae50abb971ca3c70cc652cae3e3da98818c48
ThuZenken <thu.nv@zenkenplanet-vn.com>
2020-09-23 13:07:30 +0700

    Open close Form AddOrEdit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super(props);
     this.state = {
       task: [],
+      isDisplayForm: false,
     };
   }
   componentWillMount() {
@@ -52,8 +53,25 @@ class App extends Component {
     return this.s4() + this.s4() + "-" + this.s4();
   }
 
+  onToggleForm = () => {
+    this.setState({
+      isDisplayForm: !this.state.isDisplayForm,
+    });
+  };
+
+  onCloseForm = () => {
+    this.setState({
+      isDisplayForm: false,
+    });
+  };
+
   render() {
-    var { task } = this.state;
+    var { task, isDisplayForm } = this.state;
+    var elmTaskForm = isDisplayForm ? (
+      <TaskForm onCloseForm={this.onCloseForm} />
+    ) : (
+      ""
+    );
     return (
       <div className="container">
         <div className="text-center">
@@ -61,21 +79,28 @@ class App extends Component {
           <hr />
         </div>
         <div className="row">
-          <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <TaskForm />
+          <div
+            className={
+              isDisplayForm ? "col-xs-4 col-sm-4 col-md-4 col-lg-4" : ""
+            }
+          >
+            {elmTaskForm}
           </div>
-          <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+          <div
+            className={
+              isDisplayForm
+                ? "col-xs-8 col-sm-8 col-md-8 col-lg-8"
+                : "col-xs-12 col-sm-12 col-md-12 col-lg-12"
+            }
+          >
             <div className="SetMb">
-              <button type="button" className="btn btn-primary mb-5 mr-5">
-                <span className="fa fa-plus mr-5" />
-                Add Task
-              </button>
               <button
                 type="button"
-                className="btn btn-danger ml-5"
-                onClick={this.GenerateData}
+                className="btn btn-primary mb-5 mr-5"
+                onClick={this.onToggleForm}
               >
-                Generate
+                <span className="fa fa-plus mr-5" />
+                Add Task
               </button>
             </div>
             <Control />
--- my-app/src/Components/TaskForm.js
+++ my-app/src/Components/TaskForm.js
@@ -1,20 +1,66 @@
 import React, { Component } from "react";
 
 class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      status: false,
+    };
+  }
+
+  onChange = (event) => {
+    var target = event.target;
+    var name = target.name;
+    var value = target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+  onCloseForm = () => {
+    this.props.onCloseForm();
+  };
   render() {
     return (
       <div className="panel panel-warning ">
         <div className="panel-heading">
-          <h3 className="panel-title">Add Task</h3>
+          <div className="row">
+            <div className="iconClose">
+              <span
+                className="fa fa-times-circle"
+                onClick={this.onCloseForm}
+              ></span>
+            </div>
+            <div className="text-left">
+              <h3 className="panel-title">Add Task</h3>
+            </div>
+          </div>
         </div>
         <div className="panel-body">
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group">
               <label>Name :</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                value={this.state.name}
+                onChange={this.onChange}
+              />
             </div>
             <label>Status :</label>
-            <select className="form-control" required="required">
+            <select
+              className="form-control"
+              required="required"
+              name="status"
+              value={this.state.status}
+              onChange={this.onChange}
+            >
               <option value={1}>Activited</option>
               <option value={0}>Hidden</option>
             </select>
