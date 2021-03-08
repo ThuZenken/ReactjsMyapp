2083475122d917fd9f88ed90932bb01bb637087c
ThuZenken <thu.nv@zenkenplanet-vn.com>
2021-03-08 16:33:30 +0700

    add link FB

--- my-app/src/Components/footer.js
+++ my-app/src/Components/footer.js
@@ -38,12 +38,12 @@ class Control extends Component {
                     <div className="row">
                         <div className="col-md-8 col-sm-6 col-xs-12">
                             <p className="copyright-text">Copyright © 2021 All Rights Reserved by
-              <a href="#"> Nguyen Van Thu</a>.
+              <a href="https://www.facebook.com/profile.php?id=100007462455263"> Nguyen Van Thu</a>.
             </p>
                         </div>
                         <div className="col-md-4 col-sm-6 col-xs-12">
                             <ul className="social-icons">
-                                <li><a className="facebook" href="#"><i className="fa fa-facebook" /></a></li>
+                                <li><a className="facebook" href="https://www.facebook.com/profile.php?id=100007462455263"><i className="fa fa-facebook" /></a></li>
                                 <li><a className="twitter" href="#"><i className="fa fa-twitter" /></a></li>
                                 <li><a className="dribbble" href="#"><i className="fa fa-dribbble" /></a></li>
                                 <li><a className="linkedin" href="#"><i className="fa fa-linkedin" /></a></li>
